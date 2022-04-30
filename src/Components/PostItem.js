45ada07e239c408018cbfb40e66db43261a2dbad
Ritesh <ritesh.patel@daffodilsw.com>
2022-04-30 15:58:34 +0530

    changes

--- src/Components/PostItem.js
+++ src/Components/PostItem.js
@@ -4,6 +4,7 @@ import { TextInput } from 'react-native';
 import { ScrollView } from 'react-native';
 import { useState } from 'react';
 import medFetch from '../Actions/fetch';
+import CrossPlatformIcon from 'react-native-cross-platform-icons';
 import { loadPost } from '../Screens/Post';
  const PostItem = (props) => {
   const {item,PostItems,setPostItems,setModalVisible}=props;
@@ -20,7 +21,7 @@ import { loadPost } from '../Screens/Post';
       </Text>
       <Image source={{uri:"https://raw.githubusercontent.com/riteshp112/Responsive-Resume/master/assets/img/logo.png"}} style={{height:75,width:'100%',resizeMode:'contain'}}>
       </Image>
-      <View style={{flexDirection:'row',flex:1,alignItems:'center',}}>
+      <View style={{flexDirection:'row',flex:1,alignItems:'center',justifyContent:'center',marginTop:10}}>
       <TouchableOpacity onPress={()=>{medFetch({
         type:'update',
         table:'post',
@@ -31,13 +32,19 @@ import { loadPost } from '../Screens/Post';
         loadPost(setPostItems, setModalVisible, PostItems)        
       }, 500); 
       }}>
-      <Text style={{flex:1,marginLeft:15}}>Likes {item?.likes}</Text>
+        <View style={{flex:1,flexDirection:'row',justifyContent:'center',alignContent:'center',alignItems:'center',}}>
+      <CrossPlatformIcon name='thumbs-up'size={15}></CrossPlatformIcon>
+      <Text style={{marginLeft:10,}}>{item?.likes}</Text>
+      </View>
       </TouchableOpacity> 
       <TouchableOpacity onPress={()=>{
         setShowComments(!showComments)
       }} >
-     <Text style={{flex:1,marginLeft:15}}>Comments {item?.comments.length}</Text>
-     </TouchableOpacity>
+      <View style={{paddingLeft:20,flex:1,flexDirection:'row',justifyContent:'center',alignContent:'center',alignItems:'center',}}>
+        <CrossPlatformIcon name='chatbubbles'size={15}></CrossPlatformIcon>
+        <Text style={{marginLeft:10,}}>{item?.comments.length}</Text>
+      </View>     
+      </TouchableOpacity>
      <TouchableOpacity onPress={()=>{medFetch({
         type:'update',
         table:'post',
@@ -48,20 +55,23 @@ import { loadPost } from '../Screens/Post';
         loadPost(setPostItems, setModalVisible, PostItems)        
       }, 500); 
       }}>
-     <Text style={{flex:1,marginLeft:15}}>Dislikes {item?.dislikes}</Text>
-     </TouchableOpacity>   
+      <View style={{flex:1,flexDirection:'row',marginLeft:20, justifyContent:'center',alignContent:'center',alignItems:'center',}}>
+      <CrossPlatformIcon name='thumbs-down' size={15}></CrossPlatformIcon>
+      <Text style={{marginLeft:10}}>{item?.dislikes}</Text>
+      </View>     
+      </TouchableOpacity>   
        </View>
       {showComments?
       <View>
         <View style={{maxHeight:80}}>
-        <ScrollView style={{flexGrow:0}}>
+        <ScrollView style={{flexGrow:0}} nestedScrollEnabled={true}>
         {commentComponent}
         </ScrollView>
         </View>
       <View style={{flexDirection:'row',flex:1}}>
         <TextInput multiline={true} value={newComment} style={{borderWidth:2,flex:1,minHeight:30}} onChangeText={(value)=>{setNewComment(value)}}>
       </TextInput>
-      <TouchableOpacity onPress={()=>{medFetch({
+      <TouchableOpacity onPress={()=>{newComment && medFetch({
         type:'update',
         table:'post',
         id:item?._id,
@@ -71,8 +81,11 @@ import { loadPost } from '../Screens/Post';
       setTimeout(() => {
         loadPost(setPostItems, setModalVisible, PostItems)        
       }, 500); 
-      }}>
-      <Text style={{paddingLeft:10}}>{'>>'}</Text>
+      }}
+      style={{alignSelf:'center'}}>
+      <View style={{paddingLeft:10}}>
+        <CrossPlatformIcon name='send' size={30} outline></CrossPlatformIcon>
+      </View>
       </TouchableOpacity>
       </View>
       </View>:void 0}
--- src/Components/RecordItem.js
+++ src/Components/RecordItem.js
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native';
 import moment from 'moment';
 import { TouchableOpacity } from 'react-native';
+import CrossPlatformIcon from 'react-native-cross-platform-icons';
 export const RecordItem = (props) => {
   const d = new Date(props.date)
   const {deleteRecords,index,setRecords}=props;
@@ -8,10 +9,11 @@ export const RecordItem = (props) => {
     
     <View style={{backgroundColor:'yellow', borderWidth:10, borderColor:'cyan',marginTop: 4 ,marginBottom:4,padding:10,borderRadius:20,}}>
      <View style={{position:'absolute',right:10}}>
-       <TouchableOpacity onPress={()=>{
+       <TouchableOpacity style={{paddingTop:5}} onPress={()=>{
          deleteRecords(index,setRecords)
-       }}><Text>X</Text>
-      </TouchableOpacity>
+       }}>
+        <CrossPlatformIcon name='trash' size={20}></CrossPlatformIcon>
+       </TouchableOpacity>
       </View>
       <Text style={{ fontSize: 25 }}>{props.type}</Text>
       <Text style={{ fontSize: 25 }}>{props.val}</Text>
