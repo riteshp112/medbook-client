69e316a4c63b293499738863ea89d1f39fa3207d
Ritesh <ritesh.patel@daffodilsw.com>
2022-07-16 12:14:34 +0530

    changes

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/Screens/Home'
 export default function App() {
   return (
@@ -7,11 +5,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
