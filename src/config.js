f4e69b320346f0e6ab8d6464d660489b3b06f2ad
Ritesh <ritesh.patel@daffodilsw.com>
2022-09-18 17:10:10 +0530

    new changes

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 let fetchURL = "http://127.0.0.1:5000/invoke"; //local
 // fetchURL="http://10.0.2.2:5000/invoke";// local emulator
-// fetchURL = "https://medbook1.herokuapp.com/invoke"; //live
+fetchURL = "https://medbook1.herokuapp.com/invoke"; //live
 // fetchURL='http://127.0.0.1:2020/invoke'//local-node
 
 let mailURL = "https://medbook1.herokuapp.com/sendMail"; // live
-mailURL = "http://127.0.0.1:5000/sendMail"; //local
+// mailURL = "http://127.0.0.1:5000/sendMail"; //local
 
 export { fetchURL, mailURL };
