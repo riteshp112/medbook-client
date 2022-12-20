5ef50727c738675051fca475d6b6d1505292476b
Ritesh <ritesh.patel@daffodilsw.com>
2022-12-20 15:29:54 +0530

    MedList

--- src/MedBook/Settings/Settings.js
+++ src/MedBook/Settings/Settings.js
@@ -1,33 +1,42 @@
 import { FlatList, View } from "react-native";
 import React from "react";
 import SettingItem from "./SettingItem";
-import { appVersion, contact, copyright, requestAndReport, share, terms } from "../../Images";
+import {
+  appVersion,
+  contact,
+  copyright,
+  requestAndReport,
+  share,
+  terms,
+} from "../../Images";
 import deviceInfoModule from "react-native-device-info";
+import { version } from "../../../package.json";
 const settingData = [
   {
     title: "Request & Report",
     subTitle: "Send Request & Report",
-    icon: requestAndReport
+    icon: requestAndReport,
   },
   {
     title: "App Version",
-    subTitle: deviceInfoModule.getVersion(),
-    icon: appVersion
+    subTitle: version,
+    icon: appVersion,
   },
   {
     title: "Contact Email",
     subTitle: "riteshp112@gmail.com",
-    icon: contact
+    icon: contact,
   },
   {
     title: "Copyright",
-    subTitle: "No Copyright Intended. Open to use, copy, modify, or distribute.",
-    icon: copyright
+    subTitle:
+      "No Copyright Intended. Open to use, copy, modify, or distribute.",
+    icon: copyright,
   },
   {
     title: "Share",
     subTitle: "Share This App",
-    icon: share
+    icon: share,
   },
   {
     title: "Terms & Policies",
@@ -38,15 +47,15 @@ const settingData = [
 
 const Settings = ({ ...restProps }) => {
   return (
-    <View style={{ flex: 1, height: '100%', backgroundColor: '#ffffff' }}>
+    <View style={{ flex: 1, height: "100%", backgroundColor: "#ffffff" }}>
       <FlatList
         data={settingData}
         renderItem={(props) => <SettingItem {...props} {...restProps} />}
-        ItemSeparatorComponent={() =>
+        ItemSeparatorComponent={() => (
           <View
             style={{ borderBottomColor: "lightgray", borderBottomWidth: 1 }}
           ></View>
-        }
+        )}
       />
     </View>
   );
