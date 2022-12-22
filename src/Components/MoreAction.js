523b5f33929d7558809d94dce2cfcc1dbe32c3fb
Ritesh <riteshp112@gmail.com>
2022-12-22 18:50:17 +0530

    MedList

--- src/Components/MoreAction.js
+++ src/Components/MoreAction.js
@@ -1,9 +1,10 @@
 import { Image, Modal, Text, TouchableOpacity, View } from "react-native";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { threeDots } from "../Images";
 
 const MoreAction = (props) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const actionRef = useRef(null);
   const {
     actions = [],
     style = {},
@@ -22,45 +23,60 @@ const MoreAction = (props) => {
       <TouchableOpacity
         onPress={(e) => {
           onPress(e);
+          setIsModalVisible((prev) => !prev);
         }}
-        onLongPress={() => {
-          alert("Long Pressed");
+        style={{
+          paddingVertical: 8,
+          paddingHorizontal: 8,
+          // backgroundColor: "red",
+          width: 150,
+          ...containerStyle,
         }}
-        style={{ ...containerStyle }}
         key={index}
       >
         <Text style={{ ...textStyle }}>{label}</Text>
       </TouchableOpacity>
     );
   });
+  return void 0;
   return (
     <View style={{ ...iconContainerStyle }}>
       <Modal
         visible={isModalVisible}
         transparent={true}
-        presentationStyle="pageSheet"
+        style={{
+          margin: 20,
+          backgroundColor: "white",
+          borderRadius: 20,
+          padding: 35,
+          alignItems: "center",
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+          elevation: 5,
+          borderWidth: 1,
+          borderColor: "#ddd",
+          ...style,
+          // margin: 0,
+          // flexDirection: "column",
+          // position: "absolute",
+          // borderRadius: 8,
+          // top: 20,
+          // left: -140,
+          backgroundColor: "white",
+          margin: 0, // This is the important style you need to set
+          alignItems: undefined,
+          justifyContent: undefined,
+        }}
       >
-        <View
-          style={{ height: "100%" }}
-          onTouchEnd={(e) => {
-            setIsModalVisible((prev) => !prev);
-            e.stopPropagation();
-          }}
-        >
-          <View
-            style={{
-              backgroundColor: "#ffffff",
-              borderWidth: 1,
-              borderColor: "#ddd",
-              flexDirection: "column",
-              ...style,
-            }}
-          >
-            {componentsToRender}
-          </View>
-        </View>
+        {componentsToRender}
       </Modal>
       <TouchableOpacity
+        ref={actionRef}
         onPress={() => {
           setIsModalVisible((prev) => !prev);
         }}
