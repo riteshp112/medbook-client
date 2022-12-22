4d494996f5793b4edd763128eb3f244e94a817ca
Ritesh <riteshp112@gmail.com>
2022-12-22 15:10:03 +0530

    MedList

--- src/config.js
+++ src/config.js
@@ -3,11 +3,13 @@ let fetchURL = "http://127.0.0.1:5000/invoke"; //local
 // fetchURL = "https://riteshp112.pythonanywhere.com/invoke"; //live
 // fetchURL='http://127.0.0.1:2020/invoke'//local-node
 // fetchURL = "https://medbook1.herokuapp.com/invoke"
-fetchURL='https://medbook-server.azurewebsites.net/invoke'
+// fetchURL='https://medbook-server.azurewebsites.net/invoke'
+fetchURL = "https://meddbook-server.onrender.com/invoke";
 
 let mailURL = "https://riteshp112.pythonanywhere.com/sendMail"; // live
-mailURL = "http://127.0.0.1:5000/sendMail"; //local
+// mailURL = "http://127.0.0.1:5000/sendMail"; //local
 // mailURL = "https://medbook1.herokuapp.com/sendMail"
-mailURL ="https://medbook-server.azurewebsites.net/sendMail"
+// mailURL = "https://medbook-server.azurewebsites.net/sendMail";
+mailURL = "https://meddbook-server.onrender.com/sendMail";
 
 export { fetchURL, mailURL };
