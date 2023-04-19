66a68f94ca1f667e3c4668a5ad5821f648419366
Ritesh <riteshp112@gmail.com>
2023-04-19 12:49:27 +0530

    remove gitignore files

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 let fetchURL="http://127.0.0.1:5000/invoke"//local
-fetchURL='https://medbook1.herokuapp.com/invoke'//live
+fetchURL='https://medbook-server.onrender.com/invoke'//live
 // fetchURL='http://127.0.0.1:2020/invoke'//node
 export {fetchURL}
