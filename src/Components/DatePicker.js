0e6deb808efca6f84d179ac1c428f0988fb377b1
Ritesh <riteshp112@gmail.com>
2023-05-07 20:50:44 +0530

    date changes

--- src/Components/DatePicker.js
+++ src/Components/DatePicker.js
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
-import { Image, Platform, Pressable, Text, View } from "react-native";
-import RNDatePicker from "react-native-date-picker";
-import { calendarIcon } from "../Images";
+import React from "react";
 
-function DatePickerWeb({
+export default function DatePickerWeb({
   value,
   onChange,
   style,
@@ -34,55 +31,4 @@ function DatePickerWeb({
       />
     </div>
   );
-}
-
-function DatePickerNative({
-  value,
-  onChange,
-  style,
-  textStyle,
-  mode = "date",
-  iconStyle,
-  icon = calendarIcon,
-}) {
-  const [open, setOpen] = useState(false);
-  const displayValue =
-    (value?.getDate() > 9 ? value?.getDate() : "0" + value?.getDate()) +
-    "/" +
-    (value?.getMonth()+1 > 9 ? value?.getMonth()+1 : "0" + (value?.getMonth()+1)) +
-    "/" +
-    value?.getFullYear();
-  return (
-    <View style={{ ...style, justifyContent: "center" }}>
-      <View style={{ justifyContent: "space-between", flexDirection: "row" }}>
-        <Text style={{ ...textStyle, textAlignVertical: "center" }}>
-          {displayValue}
-        </Text>
-        <Pressable onPress={() => setOpen(true)}>
-          <Image
-            source={icon}
-            resizeMode="contain"
-            style={{ height: 32, width: 32, ...iconStyle }}
-          />
-        </Pressable>
-      </View>
-      <RNDatePicker
-        open={open}
-        modal
-        mode={mode}
-        date={value}
-        onConfirm={(props) => {
-          onChange && onChange(props);
-          setOpen(false);
-        }}
-        onCancel={() => {
-          setOpen(false);
-        }}
-      />
-    </View>
-  );
-}
-
-export default function DatePicker(props) {
-  return Platform.OS === "web" ? DatePickerWeb(props) : DatePickerNative(props);
 }
